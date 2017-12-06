c48e773af674d2bb3ff2519da74bc41f2622f0fb
makiroggers <makiroggers@gmail.com>
2017-12-05 23:52:08 -0800

    More working, less breaking

--- client/views/error/ErrorPage.js
+++ client/views/error/ErrorPage.js
@@ -4,38 +4,44 @@ import history from '../../history';
 import Link from '../../components/Link';
 import s from './ErrorPage.css';
 
-class ErrorPage extends React.Component {
 
+class ErrorPage extends React.Component {
   static propTypes = {
-    error: PropTypes.object,
-  };
+    error: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  }
 
   componentDidMount() {
     document.title = this.props.error && this.props.error.status === 404 ?
       'Page Not Found' : 'Error';
   }
 
-  goBack = event => {
+  goBack = (event) => {
     event.preventDefault();
     history.goBack();
   };
 
   render() {
-    // eslint-disable-next-line no-console
-    if (this.props.error && console.error) console.error(this.props.error);
+    if (this.props.error && console.error) { // eslint-disable-line no-console
+      console.error(this.props.error); // eslint-disable-line no-console
+    }
 
     const [code, title] = this.props.error && this.props.error.status === 404 ?
       ['404', 'Page not found'] :
-      ['Error', 'Oups, something went wrong'];
+      ['Error', 'Oops, something went wrong!'];
 
     return (
       <div className={s.container}>
         <main className={s.content}>
           <h1 className={s.code}>{code}</h1>
-          <p className={s.title}>{title}</p>
-          {code === '404' && <p className={s.text}>
-            The page you are looking for does not exist or an another error occurred.
-          </p>}
+          <p className={s.title}>
+            {title}
+          </p>
+          {
+            code === '404' &&
+            <p className={s.text}>
+              The page you are looking for does not exist or an another error occurred.
+            </p>
+          }
           <p className={s.text}>
             <a href="/" onClick={this.goBack}>Go back</a>, or head over to the&nbsp;
             <Link to="/">home page</Link> to choose a new direction.
