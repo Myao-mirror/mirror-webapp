e36c66842b06a27e7ae896b624e8c3ade649b0a8
makiroggers <makiroggers@gmail.com>
2017-12-07 23:02:44 -0800

    Removing unnecessary code and updated styling for high contrast

--- client/views/about/About.js
+++ client/views/about/About.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '../../components/Layout';
 
 class About extends React.Component {
-
   componentDidMount() {
     document.title = 'About Me';
   }
@@ -12,22 +11,21 @@ class About extends React.Component {
       <Layout>
         <div className="mdl-grid">
           <div className="mdl-cell mdl-cell--6-col">
-            <h1 className="mdl-typography--title">About Me</h1>
+            <h1 className="mdl-typography--title">About Mirror</h1>
             <p className="mdl-typography--body-1">
               Coming soon.
             </p>
           </div>
           <div className="mdl-cell mdl-cell--6-col">
-            <h1 className="mdl-typography--title">About Me</h1>
+            <h1 className="mdl-typography--title">Mirror Stuff</h1>
             <p className="mdl-typography--body-1">
-              Coming soon.
+              This mirror is the most awesome mirror ever.
             </p>
           </div>
         </div>
       </Layout>
     );
   }
-
 }
 
 export default About;
