8f96a05f2946b1a063e0f7f4734b398136e5e0b6
ayuspark <lola.luyaowang@gmail.com>
2017-12-10 00:34:37 -0800

    added redux-logger to test store async call, hope not to break the template

--- client/store.js
+++ client/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
-
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
+import axios from 'axios';
 
 // Centralized application state
 // For more information visit http://redux.js.org/
@@ -21,8 +21,17 @@ const logger = (store) => (next) => (action) => {
 };
 const middleware = applyMiddleware(logger);
 
+const weatherInitialState = {
+  fetching: false,
+  fetched: false,
+  weather: [],
+  err: null,
+  data: null,
+  res: null,
+};
+
 // create in diff files, , then import here
-const weatherReducer = (state = {}, action) => {
+const weatherReducer = (state = weatherInitialState, action) => {
   switch (action.type) {
     case 'CHANGE_CITY': {
       state = { ...state, city: action.payload };
@@ -31,6 +40,17 @@ const weatherReducer = (state = {}, action) => {
     case 'CHANGE_TEMP': {
       state = { ...state, temp: action.payload };
       break;
+    }
+    case 'REQUEST_WEATHER': {
+      state = { ...state, data: action.payload };
+      break;
+    }
+    case 'RECEIVE_WEATHER': {
+      state = { ...state, res: action.payload };
+      break;
+    }
+    case 'WEATHER_ERROR': {
+      state = { ...state, err: action.payload };
     }
     default:
       return state;
@@ -55,7 +75,22 @@ store.subscribe(() => {
   console.log('************** store changed', store.getState());
 });
 
-store.dispatch({ type: 'CHANGE_CITY', payload: 'Shanghai' });
-store.dispatch({ type: 'CHANGE_TEMP', payload: '33' });
+// these are the example to dispacth single actions one by one
+// store.dispatch({ type: 'CHANGE_CITY', payload: 'Shanghai' });
+// store.dispatch({ type: 'CHANGE_TEMP', payload: '33' });
+
+
+// dispatch things together ASYNC
+store.dispatch((dispatch) => {
+  dispatch({ type: 'REQUEST_WEATHER' });
+  let apiKey = '91989caa792ff47f0d8628d457e8864b';
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=seattle&appid=${apiKey}`)
+    .then((res) => {
+      dispatch({ type: 'RECEIVE_WEATHER', payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: 'WEATHER_ERROR', payload: err });
+    });
+});
 
 export default store;
