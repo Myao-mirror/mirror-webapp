cc48c87ffda8f743ffd4b4e06635c996235b3080
ayuspark <lola.luyaowang@gmail.com>
2017-12-13 22:37:33 -0800

    took out the test component, and its residue in landing.js

--- client/views/Landing/Landing.js
+++ client/views/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import * as firebase from 'firebase';
+
 import fire from '../../Firebase/setup';
 
 import Layout from '../../components/Layout';
@@ -8,8 +8,6 @@ import Time from '../../components/Time/Time';
 import News from '../../components/News/News';
 import App from '../../components/Pet/PetComponent';
 import Weather from '../../components/Weather/Weather';
-import Test from '../../components/Time/Test';
-import { testPostDisplayRequest, testDisplayAction } from '../../actions/testActions';
 
 // TODO: get rid of all the test
 class Landing extends React.Component {
@@ -44,34 +42,13 @@ class Landing extends React.Component {
     clearInterval(this.interval);
   }
 
-  getUpdatedDisplay() {
-    this.props.dispatch(testDisplayAction());
-    // console.log('TEST FIRED!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-  }
-
-  toUpdateDisplay() {
-    let bool = true;
-    if (this.props.test.testState.displayBool !== undefined) {
-      bool = !this.props.test.testState.displayBool;
-    }
-    this.props.dispatch(testPostDisplayRequest(bool));
-  }
-
   render() {
-    let testDisplayBoolFromStore = false;
-    if (this.props.test.testState.displayBool !== undefined) {
-      testDisplayBoolFromStore = this.props.test.testState.displayBool;
-    }
     return (
       <Layout>
         <Time />
         { this.state.fireNews ? <News /> : null }
         <App />
         <Weather />
-        {/* <h1>fireData is: {this.state.fireData.news}</h1> */}
-        <button onClick={this.toUpdateDisplay}>Toggle Test Component</button>
-        <h4>testDisplayBoolFromStore: {testDisplayBoolFromStore.toString()}</h4>
-        {testDisplayBoolFromStore ? <Test /> : null}
       </Layout>
     );
   }
