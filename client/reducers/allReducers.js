3feb8222ea822acbab11f3339d062eae9c114984
makiroggers <makiroggers@gmail.com>
2017-12-15 08:51:45 -0800

    Removed all files to fix directories

--- client/reducers/allReducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import weatherReducer from './weatherReducer';
-
-
-const reducers = combineReducers({
-  weather: weatherReducer, // what data we modify(weather), using what(weatherReducer)
-});
-
-export default reducers;
--- server/Models/User.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-
-namespace Server.Models
-{
-    public class User : IdentityUser
-    {}
-}
