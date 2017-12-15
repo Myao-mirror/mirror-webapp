98ddda4d4c256c7c8467c4135e357497c5a4b39e
ayuspark <lola.luyaowang@gmail.com>
2017-12-14 23:11:07 -0800

    added a weather setting func to test

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,9 @@ admin.initializeApp(functions.config().firebase);
 const dbRoot = admin.database().ref('/voice-pi');
 
 // INTENT NAMES
-// 🔥 IMPORTANT: make sure the INTENT NAME is exactly the same as ACTION NAME, in this case, the function name below. e.g: INTENT NAME is 'displayAll', function name is 'displayAll'. Otherwise it cause error: no matching intent handler for: <intent name>.
+// 🔥 IMPORTANT: make sure the INTENT NAME is exactly the same as ACTION NAME(aka the function name below). e.g: INTENT NAME is 'displayAll', function name is 'displayAll'. Otherwise it cause error: no matching intent handler for: <intent name>.
 const DISPLAY_ALL_INTENT = 'displayAll';
+const WEATHER_INTENT = 'weatherSettings';
 
 // CONTEXT PARAMETERS
 const USERNAME_PARAM = 'username';
@@ -20,25 +21,41 @@ const DISPLAY_BOOL_PARAM = 'bool';
 exports.myaoMirrorWebhook = functions.https.onRequest((req, res) => {
   const assistant = new Assistant({ request: req, response: res });
 
+  // MAP INTENTS TO ACTIONS
   const actionMap = new Map();
   actionMap.set(DISPLAY_ALL_INTENT, displayAll);
-//   actionMap.set(ACTION_INTENT, action_to_display);
+  actionMap.set(WEATHER_INTENT, weatherSettings);
   assistant.handleRequest(actionMap);
 
+  // GET USER INFORMATION TO USE IN THE FUNC BELOW
+  const username = req.body.result.parameters[USERNAME_PARAM];
+  const component = req.body.result.parameters[COMPONENT_PARAM];
+  const displayBool = req.body.result.parameters[DISPLAY_BOOL_PARAM];
+  const user = dbRoot.child(username);
+
+  // ACTION TO DISPLAY ALL COMPONENT
   function displayAll(assistant) {
-    const username = req.body.result.parameters[USERNAME_PARAM];
-    const displayBool = req.body.result.parameters[DISPLAY_BOOL_PARAM];
-    const user = dbRoot.child(username);
-    
-    let displayUpdates = {};
+    const displayUpdates = {};
     displayUpdates['/news/settings/active'] = displayBool;
     displayUpdates['/time/settings/active'] = displayBool;
     displayUpdates['/weather/settings/active'] = displayBool;
     displayUpdates['/pet/settings/active'] = displayBool;
-
     user.update(displayUpdates);
+
     const status = displayBool ? 'Here are all the good stuff for you!' : 'Here is an empty mirror for you!';
     const speech = `Hey ${username}! ${status}`;
+    assistant.ask(speech);
+  }
+
+  // TODO:  EXPAND WEATHER ACTIONS
+  function weatherSettings(assistant) {
+    const settingUpdates = {};
+    const path = `/${component}/settings/active`;
+    settingUpdates[path] = displayBool;
+    user.update(settingUpdates);
+
+    const status = displayBool ? 'on' : 'off';
+    const speech = `I turned ${status} ${component} for you!`;
     assistant.ask(speech);
   }
 });
