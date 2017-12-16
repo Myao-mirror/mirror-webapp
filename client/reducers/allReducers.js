0b24e2ca8dbf30df8de407131d7aa8a12c28dfa3
Maki Roggers <18458620+makiroggers@users.noreply.github.com>
2017-12-16 09:41:41 -0800

    Add files via upload

---
+++ client/reducers/allReducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import weatherReducer from './weatherReducer';
+
+
+const reducers = combineReducers({
+  weather: weatherReducer, // what data we modify(weather), using what(weatherReducer)
+});
+
+export default reducers;
---
+++ test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('test', () => {
+
+  it('should pass', () => {
+    expect(1).to.be.ok;
+  });
+
+});
